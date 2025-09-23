68f496ab70e12a17b974f0bf1a357d52105e060c
mogumoguippai-ekth <mogumogu.ippai@gmail.com>
2025-09-23 17:37:31 +0900

    a-9 submission

---
+++ a-9.py
@@ -0,0 +1,17 @@
+# 課題制作中
+
+"""
+下記のコードが期待通り動作するような辞書を定義する
+print(bob_info["first_name"]) # "Bob"
+print(bob_info["family_name"]) # "Dylan"
+print(bob_info["age"]) # 79
+"""
+
+# 辞書型は、 辞書名 = {"キー1": "値1", "キー2": "値2", ...}
+
+bob_info = {"first_name": "Bob", "family_name": "Dylan", "age": 79}
+
+
+print(bob_info["first_name"]) # "Bob"
+print(bob_info["family_name"]) # "Dylan"
+print(bob_info["age"]) # 79
